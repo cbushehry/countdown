4b38b2187ff7e38486774648b83da654a732cb90
Cameron Bushehry <c.bushehry@gmail.com>
2022-05-09 10:23:43 -0700

    minor refactor, application complete for now

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,12 @@ var counter = 10
 var countdown = function (){
  console.log(counter);
  counter--;
-    if(counter === 0){
-        console.log("blastoff!");
+    if(counter === 5){
+        console.log("5 seconds until launch!")
+    };
+
+    if(counter === -1){
+        console.log("blast off!");
         clearInterval(startCountdown);
     };
 };
