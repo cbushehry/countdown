48093b4d87eeba95cb9e1220affdc2c6baf1e946
Cameron Bushehry <c.bushehry@gmail.com>
2022-05-09 10:15:54 -0700

    countdown now stops when it reaches 0, inital counter changed to 10

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
-var counter = 15
+var counter = 10
 
 var countdown = function (){
  console.log(counter);
  counter--;
     if(counter === 0){
         console.log("blastoff!");
-    }
-}
+        clearInterval(startCountdown);
+    };
+};
 
 var startCountdown = setInterval(countdown, 1000);
