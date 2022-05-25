10bb48bd869db93bbe54e72e55713d7a5020af9d
Cameron Bushehry <c.bushehry@gmail.com>
2022-05-25 11:12:55 -0700

    minor refactor to add a bit more depth to countdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,24 @@
-var counter = 100
+var counter = 70
 
 var countdown = function (){
  console.log(counter);
  counter--;
+     if(counter === 60){
+        console.log("1 Minute until launch!")
+    };
+
     if(counter === 10){
-        console.log("5 seconds until launch!")
+        console.log("10 seconds until launch!")
     };
 
     if(counter === -1){
-        console.log("blast off!");
+        console.log("BLAST OFF!")
+    }
+
+    if(counter === -3){
+        console.log("Refresh the page to restart the counter!");
         clearInterval(startCountdown);
     };
 };
 
-var startCountdown = setInterval(countdown, 500);
+var startCountdown = setInterval(countdown, 200);
