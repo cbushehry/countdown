ca823a532cb505b42947840d3a3b79ada04b524d
Cameron Bushehry <c.bushehry@gmail.com>
2022-09-16 14:08:32 -0700

    minor edit to countdown timer, switched console.log to document.write

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,56 @@
-var counter = 70
+var counter = 65
 
 var countdown = function (){
  console.log(counter);
  counter--;
-     if(counter === 60){
-        console.log("1 Minute until launch!")
-    };
 
-    if(counter === 10){
-        console.log("10 seconds until launch!")
-    };
-
-    if(counter === -1){
-        console.log("BLAST OFF!")
+    if(counter === 60){
+        document.write("60 seconds until launch!");
     }
 
-    if(counter === -3){
-        console.log("Refresh the page to restart the counter!");
+    if(counter === 50){
+        document.write("<br>");
+        document.write("50 seconds until launch!");
+    }
+    
+    if(counter === 40){
+        document.write("<br>");
+        document.write("40 seconds until launch!");
+    }
+
+    if(counter === 30){
+        document.write("<br>");
+        document.write("30 seconds until launch!");
+    }
+
+    if(counter === 20){
+        document.write("<br>");
+        document.write("20 seconds until launch!");
+    }
+
+    if(counter === 10){
+        document.write("<br>");
+        document.write("10 seconds until launch!");
+    }
+
+    if(counter === 3){
+        document.write("<br>");
+        document.write("3 seconds until launch!");
+    }
+
+    if(counter === 0){
+        document.write("<br>");
+        document.write("<br>");
+        document.write("BLAST OFF!");
+    }
+
+    if(counter === -5){
+        document.write("<br>");
+        document.write("<br>");
+        document.write("Refresh the page to restart the counter!");
+
         clearInterval(startCountdown);
-    };
-};
+    }
+}
 
 var startCountdown = setInterval(countdown, 1000);
